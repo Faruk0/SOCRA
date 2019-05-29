9367f89e98c514703ed5b7d53efa59d453216e90
Valentin Simon <valentin.simon@epita.fr>
2019-05-29 19:24:40 +0200

    renamed test it2

--- src/test/java/com/epita/socra/app/MorsetoArabicTest.java
+++ src/test/java/com/epita/socra/app/MorsetoArabicTest.java
@@ -7,35 +7,35 @@ import static org.mockito.Mockito.mock;
 
 public class MorsetoArabicTest {
     @Test
-    public void given_10_check_value_of_10_in_morse() {
+    public void given_10_check_value_of_10_in_arabic() {
         IOAdapter mock = mock(IOAdapter.class);
         MorsetoArabic morse = new MorsetoArabic(mock);
         int result = morse.convert(". _ _ _ _ _ _ _ _ _");
         assert result == 10;
     }
     @Test
-    public void given_1903_check_value_of_1903_in_morse() {
+    public void given_1903_check_value_of_1903_in_arabic() {
         IOAdapter mock = mock(IOAdapter.class);
         MorsetoArabic morse = new MorsetoArabic(mock);
         int result = morse.convert(". _ _ _ _ _ _ _ _ . _ _ _ _ _ . . . _ _");
         assert result == 1903;
     }
     @Test
-    public void given_83_check_value_of_83_in_morse() {
+    public void given_83_check_value_of_83_in_arabic() {
         IOAdapter mock = mock(IOAdapter.class);
         MorsetoArabic morse = new MorsetoArabic(mock);
         int result = morse.convert("_ _ _ . . . . . _ _");
         assert result == 83;
     }
     @Test
-    public void given_42_check_value_of_42_in_morse() {
+    public void given_42_check_value_of_42_in_arabic() {
         IOAdapter mock = mock(IOAdapter.class);
         MorsetoArabic morse = new MorsetoArabic(mock);
         int result = morse.convert(". . . . _ . . _ _ _");
         assert result == 42;
     }
     @Test
-    public void given_999_check_value_of_999_in_morse() {
+    public void given_999_check_value_of_999_in_arabic() {
         IOAdapter mock = mock(IOAdapter.class);
         MorsetoArabic morse = new MorsetoArabic(mock);
         int result = morse.convert("_ _ _ _ . _ _ _ _ . _ _ _ _ .");
@@ -43,7 +43,7 @@ public class MorsetoArabicTest {
     }
 
     @Test
-    public void given_567_check_value_of_567_in_morse() {
+    public void given_567_check_value_of_567_in_arabic() {
         IOAdapter mock = mock(IOAdapter.class);
         MorsetoArabic morse = new MorsetoArabic(mock);
         int result = morse.convert(". . . . . _ . . . . _ _ . . .");
